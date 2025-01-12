348fb9421b702d11caa2c7ec014a489deee66983
Gouravkumarpandey <kumarpandeygourav@gmail.com>
2025-01-12 16:57:12 +0530

    added

---
+++ pointers/pointers.cpp
@@ -0,0 +1,15 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+    float price = 100.25;
+    float*ptr = &price;
+
+    cout<< ptr<< endl;
+    cout<< &price << endl;
+
+    return 0;
+
+}
+
