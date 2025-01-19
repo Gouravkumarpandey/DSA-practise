071d4f269036b1f28b933ab3be89eba314069c1c
Gouravkumarpandey <kumarpandeygourav@gmail.com>
2025-01-19 20:20:28 +0530

    add

--- pointers/null_pointer.cpp
+++ pointers/null_pointer.cpp
@@ -1,14 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main (){
-  
-int n ;
-cin >> n;
-cout<< "enter the value of n ";
-for(int i =0;i<n;i++){
-    
+vector<int> pairSum(vector<int> nums, int target){
+ vector<int>ans;
+
+ int n= nums.size();
+
+ for(int i=0;i<n;i++)
+ {
+    for(int j =i+1;j<n;j++){
+        if()
+    }
+ }
 }
 
-  
-}
+int main (){
+    vector<int> num ={2,7,11,15}
+
+    int target =9;
+
+    for 
+    }
