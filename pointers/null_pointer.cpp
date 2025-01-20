6ccfda8e0884ce937ef13ebfa90a41223ce59509
Gouravkumarpandey <kumarpandeygourav@gmail.com>
2025-01-20 10:18:30 +0530

    add

--- pointers/null_pointer.cpp
+++ pointers/null_pointer.cpp
@@ -9,15 +9,20 @@ vector<int> pairSum(vector<int> nums, int target){
  for(int i=0;i<n;i++)
  {
     for(int j =i+1;j<n;j++){
-        if(num[i]+num[j] ==target)
+        if(nums[i]+nums[j] ==target){
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }
     }
  }
 }
 
 int main (){
-    vector<int> num ={2,7,11,15}
+    vector<int> nums ={2,7,11,15};
 
     int target =9;
-
-    for 
+    vector<int> ans = pairSum(nums, target);
+    cout<< ans[0] << " ,"<< ans[1] << endl;
+    return 0;
     }
