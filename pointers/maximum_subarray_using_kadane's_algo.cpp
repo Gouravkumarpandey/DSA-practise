fe933f80c24783998ca1efb0a11434947e14922b
Gouravkumarpandey <kumarpandeygourav@gmail.com>
2025-01-22 21:02:28 +0530

    update

--- pointers/maximum_subarray_using_kadane's_algo.cpp
+++ pointers/maximum_subarray_using_kadane's_algo.cpp
@@ -1,5 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int mai
+int main (){
+
+    int maxSubArray(vector<int>& nums){
+         
+         int curSum =0, maxSum= INT_MIN;
+
+         for(int val :nums){
+            curSum += val;
+            maxSum = max (curSum , maxSum);
+
+            if(curSum<0){
+                curSum =0;
+
+            }
+         }
+         return maxSum;
+    }
 }
