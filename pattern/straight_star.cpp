5a791a437f0cfa96e99daf9d765ae58b4d7e55f3
Gouravkumarpandey <kumarpandeygourav@gmail.com>
2025-02-06 06:30:30 +0530

    add

---
+++ pattern/straight_star.cpp
@@ -0,0 +1,23 @@
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(){
+int n;
+for(int i =0;i<5;i++){
+    for(int j=0;j<5;j++){
+        cout<< "*";
+    }
+    cout<< endl;
+}
+
+}
+int main(){
+    print();
+    return 0;
+}
