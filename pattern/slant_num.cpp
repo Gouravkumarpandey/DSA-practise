ff82803493dcb0b8810d89cfa2e92844eb2803e4
Gouravkumarpandey <kumarpandeygourav@gmail.com>
2025-02-08 20:27:02 +0530

    done

---
+++ pattern/slant_num.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    void slant_num(int n){
+        for(int i =1;i<n ;i++){
+            for(int j=1;j<=i;j++){
+                cout<<j<<" ";
+
+            }
+
+            cout<< endl;
+
+            }
+        }
+            int main (){
+                int n =5;
+                slant_num(n);
+                return 0;
+        
+            }
+        
+    
