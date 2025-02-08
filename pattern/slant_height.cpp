26bea441f528d20999533592634afa9c2bb2f73a
Gouravkumarpandey <kumarpandeygourav@gmail.com>
2025-02-08 20:19:16 +0530

    done

--- pattern/slant_height.cpp
+++ pattern/slant_height.cpp
@@ -1,6 +1,17 @@
-#include<stdio.h>
+#include <iostream>
 using namespace std;
 
-{
-    
+void slant_height(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n = 5;
+    slant_height(n);
+    return 0;
 }
