2358cbb65f6dc7cfaf67395b2c234f729365a1d8
Gouravkumarpandey <kumarpandeygourav@gmail.com>
2025-03-28 19:24:02 +0530

    added

--- .vscode/Recursion/start.cpp
+++ .vscode/Recursion/start.cpp
@@ -1,1 +1,19 @@
 #include<iostream>
+using namespace std;
+
+
+int sum(int n){
+    if(n==1)
+    return 1;
+    
+
+    return n+ sum (n-1);
+}
+int main(){
+    int n ;
+    cout<<"enter the value of n"<<endl;
+    cin>>n;
+    
+    cout<<sum(n)<<endl;
+    return 0;
+}
